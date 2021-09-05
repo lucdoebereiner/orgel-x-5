3efbc6b996a965db5e8f570a21d6b6c694fd177e
lucdoebereiner <luc.doebereiner@gmail.com>
2021-09-05 18:42:16 +0200

    build script, browser.document

--- src/Main.elm
+++ src/Main.elm
@@ -1,14 +1,58 @@
-module Main exposing (main)
+module Main exposing (Model, Msg, init, main, organ, subscriptions, title, update, view)
 
+import Browser
 import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 
 
+main : Program () Model Msg
 main =
-    layout []
-        organ
+    Browser.document
+        { view = view
+        , init = \() -> init
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+type alias Model =
+    ()
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( (), Cmd.none )
+
+
+type alias Msg =
+    ()
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view m =
+    { title = "Orgel, Orgel, Orgel, Orgel, Orgel"
+    , body =
+        [ layout
+            [ Font.size 18
+            , Font.family
+                [ Font.typeface "Inconsolata"
+                , Font.monospace
+                ]
+            ]
+            organ
+        ]
+    }
 
 
 organ : Element msg
