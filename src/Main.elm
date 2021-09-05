0407e01053e7c22e11e66d8f00a6fb3b3a4351b5
lucdoebereiner <luc.doebereiner@gmail.com>
2021-09-05 16:03:06 +0200

    bare

--- src/Main.elm
+++ src/Main.elm
@@ -1,31 +1,23 @@
-module Main exposing (main, myElement, myRowOfStuff)
+module Main exposing (main)
 
-import Element exposing (Element, alignRight, centerY, el, fill, padding, rgb255, row, spacing, text, width)
+import Element exposing (..)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
 
 
 main =
-    Element.layout []
-        myRowOfStuff
+    layout []
+        organ
 
 
-myRowOfStuff : Element msg
-myRowOfStuff =
-    row [ width fill, centerY, spacing 30 ]
-        [ myElement
-        , myElement
-        , el [ alignRight ] myElement
+organ : Element msg
+organ =
+    row [ width fill, centerX, spacing 30, padding 20 ]
+        [ title
         ]
 
 
-myElement : Element msg
-myElement =
-    el
-        [ Background.color (rgb255 240 0 245)
-        , Font.color (rgb255 255 255 255)
-        , Border.rounded 3
-        , padding 30
-        ]
-        (text "stylish!")
+title : Element msg
+title =
+    paragraph [ Font.size 20 ] [ text "Orgel, Orgel, Orgel, Orgel, Orgel" ]
